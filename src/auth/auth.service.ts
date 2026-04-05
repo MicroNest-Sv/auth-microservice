0e5a7e44488a52f459b0b2be879182ca79722f9a
s4v!0r <egbp25@outlook.com>
2026-04-04 21:23:38 -0500

    refactor(auth): improve error handling and message consistency in signUp method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { RpcException } from '@nestjs/microservices';
 import * as bcrypt from 'bcryptjs';
 
 import { PrismaService } from '@src/common/services';
+
 import { SignUpDto, SignInDto } from './dto';
 
 @Injectable()
@@ -10,29 +11,38 @@ export class AuthService {
   constructor(private readonly prisma: PrismaService) {}
 
   async signUp(signUpDto: SignUpDto) {
-    const { name, email, password } = signUpDto;
+    try {
+      const { name, email, password } = signUpDto;
 
-    const existingUser = await this.prisma.user.findUnique({
-      where: { email },
-    });
+      const existingUser = await this.prisma.user.findUnique({
+        where: { email },
+      });
 
-    if (existingUser) {
+      if (existingUser) {
+        throw new RpcException({
+          status: HttpStatus.CONFLICT,
+          messages: ['El email ya está registrado'],
+        });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const user = await this.prisma.user.create({
+        data: { name, email, password: hashedPassword },
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password: _, ...userWithoutPassword } = user;
+
+      return userWithoutPassword;
+    } catch (error) {
+      if (error instanceof RpcException) throw error;
+
       throw new RpcException({
-        status: HttpStatus.CONFLICT,
-        message: 'El email ya está registrado',
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        messages: ['Error al registrar usuario'],
       });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await this.prisma.user.create({
-      data: { name, email, password: hashedPassword },
-    });
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { password: _, ...userWithoutPassword } = user;
-
-    return userWithoutPassword;
   }
 
   signIn(signInDto: SignInDto) {
